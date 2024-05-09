50fb4ccb9a1cc97069057e9e7d5c921d38158953
maingo2005 <k023g1006@g.neec.ac.jp>
2024-05-09 19:03:26 +0900

    00_05_課題

--- main.cpp
+++ main.cpp
@@ -15,166 +15,166 @@ struct Matrix4x4 {
 	float m[4][4];
 };
 
-//平行移動行列
-Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
-	Matrix4x4 M = {};
-
-	M.m[0][0] = 1;
-	M.m[0][1] = 0;
-	M.m[0][2] = 0;
-	M.m[0][3] = 0;
-
-	M.m[1][0] = 0;
-	M.m[1][1] = 1;
-	M.m[1][2] = 0;
-	M.m[1][3] = 0;
-
-	M.m[2][0] = 0;
-	M.m[2][1] = 0;
-	M.m[2][2] = 1;
-	M.m[2][3] = 0;
-
-	M.m[3][0] = translate.x;
-	M.m[3][1] = translate.y;
-	M.m[3][2] = translate.z;
-	M.m[3][3] = 1;
-
-	return M;
-}
-
-//拡大縮小行列
-Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
-	Matrix4x4 M = {};
-
-	M.m[0][0] = scale.x;
-	M.m[0][1] = 0;
-	M.m[0][2] = 0;
-	M.m[0][3] = 0;
-
-	M.m[1][0] = 0;
-	M.m[1][1] = scale.y;
-	M.m[1][2] = 0;
-	M.m[1][3] = 0;
-
-	M.m[2][0] = 0;
-	M.m[2][1] = 0;
-	M.m[2][2] = scale.z;
-	M.m[2][3] = 0;
-
-	M.m[3][0] = 0;
-	M.m[3][1] = 0;
-	M.m[3][2] = 0;
-	M.m[3][3] = 1;
-
-	return M;
-}
-
-//１.X軸回転行列
-Matrix4x4 MakeRotateXMatrix(float radian) {
-	Matrix4x4 M;
-	M.m[0][0] = 1;
-	M.m[0][1] = 0;
-	M.m[0][2] = 0;
-	M.m[0][3] = 0;
-
-	M.m[1][0] = 0;
-	M.m[1][1] = cosf(radian);
-	M.m[1][2] = sinf(radian);
-	M.m[1][3] = 0;
-
-	M.m[2][0] = 0;
-	M.m[2][1] = -1 * sinf(radian);
-	M.m[2][2] = cosf(radian);
-	M.m[2][3] = 0;
-
-	M.m[3][0] = 0;
-	M.m[3][1] = 0;
-	M.m[3][2] = 0;
-	M.m[3][3] = 1;
-
-	return M;
-}
-//2.Y軸回転行列
-Matrix4x4 MakeRotateYMatrix(float radian) {
-	Matrix4x4 M;
-	M.m[0][0] = cosf(radian);
-	M.m[0][1] = 0;
-	M.m[0][2] = -1 * sinf(radian);
-	M.m[0][3] = 0;
-
-	M.m[1][0] = 0;
-	M.m[1][1] = 1;
-	M.m[1][2] = 0;
-	M.m[1][3] = 0;
-
-	M.m[2][0] = sinf(radian);
-	M.m[2][1] = 0;
-	M.m[2][2] = cosf(radian);
-	M.m[2][3] = 0;
-
-	M.m[3][0] = 0;
-	M.m[3][1] = 0;
-	M.m[3][2] = 0;
-	M.m[3][3] = 1;
-
-	return M;
-}
-//3.Z軸回転行列
-Matrix4x4 MakeRotateZMatrix(float radian) {
-	Matrix4x4 M;
-	M.m[0][0] = cosf(radian);
-	M.m[0][1] = sinf(radian);
-	M.m[0][2] = 0;
-	M.m[0][3] = 0;
-
-	M.m[1][0] = -1 * sinf(radian);
-	M.m[1][1] = cosf(radian);
-	M.m[1][2] = 0;
-	M.m[1][3] = 0;
-
-	M.m[2][0] = 0;
-	M.m[2][1] = 0;
-	M.m[2][2] = 1;
-	M.m[2][3] = 0;
-
-	M.m[3][0] = 0;
-	M.m[3][1] = 0;
-	M.m[3][2] = 0;
-	M.m[3][3] = 1;
-
-	return M;
-}
-
-Matrix4x4 Multiply(Matrix4x4 matrix1, Matrix4x4 matrix2) {
-	Matrix4x4 multiplication;
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			multiplication.m[i][j] = 0;
-			for (int k = 0; k < 4; k++) {
-				multiplication.m[i][j] += matrix1.m[i][k] * matrix2.m[k][j];
-			}
-		}
-	}
-	return multiplication;
-}
+////平行移動行列
+//Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+//	Matrix4x4 M = {};
+//
+//	M.m[0][0] = 1;
+//	M.m[0][1] = 0;
+//	M.m[0][2] = 0;
+//	M.m[0][3] = 0;
+//
+//	M.m[1][0] = 0;
+//	M.m[1][1] = 1;
+//	M.m[1][2] = 0;
+//	M.m[1][3] = 0;
+//
+//	M.m[2][0] = 0;
+//	M.m[2][1] = 0;
+//	M.m[2][2] = 1;
+//	M.m[2][3] = 0;
+//
+//	M.m[3][0] = translate.x;
+//	M.m[3][1] = translate.y;
+//	M.m[3][2] = translate.z;
+//	M.m[3][3] = 1;
+//
+//	return M;
+//}
+//
+////拡大縮小行列
+//Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+//	Matrix4x4 M = {};
+//
+//	M.m[0][0] = scale.x;
+//	M.m[0][1] = 0;
+//	M.m[0][2] = 0;
+//	M.m[0][3] = 0;
+//
+//	M.m[1][0] = 0;
+//	M.m[1][1] = scale.y;
+//	M.m[1][2] = 0;
+//	M.m[1][3] = 0;
+//
+//	M.m[2][0] = 0;
+//	M.m[2][1] = 0;
+//	M.m[2][2] = scale.z;
+//	M.m[2][3] = 0;
+//
+//	M.m[3][0] = 0;
+//	M.m[3][1] = 0;
+//	M.m[3][2] = 0;
+//	M.m[3][3] = 1;
+//
+//	return M;
+//}
+//
+////１.X軸回転行列
+//Matrix4x4 MakeRotateXMatrix(float radian) {
+//	Matrix4x4 M;
+//	M.m[0][0] = 1;
+//	M.m[0][1] = 0;
+//	M.m[0][2] = 0;
+//	M.m[0][3] = 0;
+//
+//	M.m[1][0] = 0;
+//	M.m[1][1] = cosf(radian);
+//	M.m[1][2] = sinf(radian);
+//	M.m[1][3] = 0;
+//
+//	M.m[2][0] = 0;
+//	M.m[2][1] = -1 * sinf(radian);
+//	M.m[2][2] = cosf(radian);
+//	M.m[2][3] = 0;
+//
+//	M.m[3][0] = 0;
+//	M.m[3][1] = 0;
+//	M.m[3][2] = 0;
+//	M.m[3][3] = 1;
+//
+//	return M;
+//}
+////2.Y軸回転行列
+//Matrix4x4 MakeRotateYMatrix(float radian) {
+//	Matrix4x4 M;
+//	M.m[0][0] = cosf(radian);
+//	M.m[0][1] = 0;
+//	M.m[0][2] = -1 * sinf(radian);
+//	M.m[0][3] = 0;
+//
+//	M.m[1][0] = 0;
+//	M.m[1][1] = 1;
+//	M.m[1][2] = 0;
+//	M.m[1][3] = 0;
+//
+//	M.m[2][0] = sinf(radian);
+//	M.m[2][1] = 0;
+//	M.m[2][2] = cosf(radian);
+//	M.m[2][3] = 0;
+//
+//	M.m[3][0] = 0;
+//	M.m[3][1] = 0;
+//	M.m[3][2] = 0;
+//	M.m[3][3] = 1;
+//
+//	return M;
+//}
+////3.Z軸回転行列
+//Matrix4x4 MakeRotateZMatrix(float radian) {
+//	Matrix4x4 M;
+//	M.m[0][0] = cosf(radian);
+//	M.m[0][1] = sinf(radian);
+//	M.m[0][2] = 0;
+//	M.m[0][3] = 0;
+//
+//	M.m[1][0] = -1 * sinf(radian);
+//	M.m[1][1] = cosf(radian);
+//	M.m[1][2] = 0;
+//	M.m[1][3] = 0;
+//
+//	M.m[2][0] = 0;
+//	M.m[2][1] = 0;
+//	M.m[2][2] = 1;
+//	M.m[2][3] = 0;
+//
+//	M.m[3][0] = 0;
+//	M.m[3][1] = 0;
+//	M.m[3][2] = 0;
+//	M.m[3][3] = 1;
+//
+//	return M;
+//}
+//
+//Matrix4x4 Multiply(Matrix4x4 matrix1, Matrix4x4 matrix2) {
+//	Matrix4x4 multiplication;
+//	for (int i = 0; i < 4; i++) {
+//		for (int j = 0; j < 4; j++) {
+//			multiplication.m[i][j] = 0;
+//			for (int k = 0; k < 4; k++) {
+//				multiplication.m[i][j] += matrix1.m[i][k] * matrix2.m[k][j];
+//			}
+//		}
+//	}
+//	return multiplication;
+//}
 
 //3次元アフィン変換行列
 Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
 	Matrix4x4 affine;
 
-	affine.m[0][0] = scale.x * 1 * std::cos(rotate.y) + 0 * 0 + 0 * std::sin(rotate.y) + 0 * 0 * std::cos(rotate.z);
-	affine.m[0][1] = scale.x * 1 * 0 + 0 * 1 + 0 * 0 + 0 * 0;
-	affine.m[0][2] = scale.x * 1 * -1 * std::sin(rotate.y) + 0 * 0 + 0 * std::cos(rotate.y) + 0 * 0 * std::sin(rotate.z);
+	affine.m[0][0] = scale.x * (((1 * std::cos(rotate.y) + 0 * 0 + 0 * std::sin(rotate.y) + 0 * 0) * std::cos(rotate.z)) + ((1 * 0 + 0 * 1 + 0 * 0 + 0 * 0) * std::sin(rotate.z)) + ((1 * std::sin(-rotate.y) + 0 * 0 + 0 * std::cos(rotate.y) + 0 * 0) * 0) + ((1 * 0 + 0 * 0 + 0 * 0 + 0 * 1) * 0));
+	affine.m[0][1] = scale.x * (((1 * std::cos(rotate.y) + 0 * 0 + 0 * std::sin(rotate.y) + 0 * 0) * std::sin(rotate.z)) + ((1 * 0 + 0 * 1 + 0 * 0 + 0 * 0) * std::cos(rotate.z)) + ((1 * std::sin(-rotate.y) + 0 * 0 + 0 * std::cos(rotate.y) + 0 * 0) * 0) + ((1 * 0 + 0 * 0 + 0 * 0 + 0 * 1) * 0));
+	affine.m[0][2] = scale.x * (((1 * std::cos(rotate.y) + 0 * 0 + 0 * std::sin(rotate.y) + 0 * 0) * 0) + ((1 * 0 + 0 * 1 + 0 * 0 + 0 * 0) * 0) + ((1 * std::sin(-rotate.y) + 0 * 0 + 0 * std::cos(rotate.y) + 0 * 0) * 1) + ((1 * 0 + 0 * 0 + 0 * 0 + 0 * 1) * 0));
 	affine.m[0][3] = 0;
 
-	affine.m[1][0] = scale.y * 0 * std::cos(rotate.y) + 1 * 0 + 0 * std::sin(rotate.y) + 0 * 0;
-	affine.m[1][1] = scale.y * 0 * 0 + 1 * 1 + 0 * 0 + 0 * 0;
-	affine.m[1][2] = scale.y * 0 * -1 * std::sin(rotate.y) + 1 * 0 + 0 * std::cos(rotate.y) + 0 * 0;
+	affine.m[1][0] = scale.y * (((0 * std::cos(rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::sin(rotate.y) + 0 * 0) * std::cos(rotate.z)) + ((0 * 0 + std::cos(rotate.x) * 1 + std::sin(rotate.x) * 0 + 0 * 0) * std::sin(-rotate.z) + ((0 * std::sin(-rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::cos(rotate.y) + 0 * 0) * 0) + ((0 * 0 + std::cos(rotate.x) * 0 + std::sin(rotate.x) * 0 + 0 * 1) * 0)));
+	affine.m[1][1] = scale.y * (((0 * std::cos(rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::sin(rotate.y) + 0 * 0) * std::sin(rotate.z)) + ((0 * 0 + std::cos(rotate.x) * 1 + std::sin(rotate.x) * 0 + 0 * 0) * std::cos(rotate.z) + ((0 * std::sin(-rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::cos(rotate.y) + 0 * 0) * 0) + ((0 * 0 + std::cos(rotate.x) * 0 + std::sin(rotate.x) * 0 + 0 * 1) * 0)));
+	affine.m[1][2] = scale.y * (((0 * std::cos(rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::sin(rotate.y) + 0 * 0) * 0) + ((0 * 0 + std::cos(rotate.x) * 1 + std::sin(rotate.x) * 0 + 0 * 0) * 0) + ((0 * std::sin(-rotate.y) + std::cos(rotate.x) * 0 + std::sin(rotate.x) * std::cos(rotate.y) + 0 * 0) * 1) + ((0 * 0 + std::cos(rotate.x) * 0 + std::sin(rotate.x) * 0 + 0 * 1) * 0));
 	affine.m[1][3] = 0;
 
-	affine.m[2][0] = scale.z * std::sin(rotate.x) * std::cos(rotate.y) + 0 * 0 + std::cos(rotate.x) * std::sin(rotate.y) + 0 * 0;
-	affine.m[2][1] = scale.z * std::sin(rotate.x) * 0 + 0 * 1 + std::cos(rotate.x) * 0 + 0 * 0;
-	affine.m[2][2] = scale.z * std::sin(rotate.x) * -1 * std::sin(rotate.y) + 0 * 0 + std::cos(rotate.x) * std::cos(rotate.y) + 0 * 0;
+	affine.m[2][0] = scale.z * (((0 * std::cos(rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::sin(rotate.y) + 0 * 0) * std::cos(rotate.z) + ((0 * 0 + std::sin(-rotate.x) * 1 + std::cos(rotate.x) * 0 + 0 * 0) * std::sin(-rotate.z)) + ((0 * std::sin(-rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::cos(rotate.y) + 0 * 0) * 0) + ((0 * 0 + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * 0 + 0 * 1) * 0)));
+	affine.m[2][1] = scale.z * (((0 * std::cos(rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::sin(rotate.y) + 0 * 0) * std::sin(rotate.z) + ((0 * 0 + std::sin(-rotate.x) * 1 + std::cos(rotate.x) * 0 + 0 * 0) * std::cos(-rotate.z)) + ((0 * std::sin(-rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::cos(rotate.y) + 0 * 0) * 0) + ((0 * 0 + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * 0 + 0 * 1) * 0)));
+	affine.m[2][2] = scale.z * (((0 * std::cos(rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::sin(rotate.y) + 0 * 0) * 0) + ((0 * 0 + std::sin(-rotate.x) * 1 + std::cos(rotate.x) * 0 + 0 * 0) * 0) + ((0 * std::sin(-rotate.y) + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * std::cos(rotate.y) + 0 * 0) * 1) + ((0 * 0 + std::sin(-rotate.x) * 0 + std::cos(rotate.x) * 0 + 0 * 1) * 0));
 	affine.m[2][3] = 0;
 
 	affine.m[3][0] = translate.x;
